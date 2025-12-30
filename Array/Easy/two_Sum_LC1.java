ae55f9727fd9045dad82f6f81590dbe82a4dff75
kushwah-Anjali <Kushwah-Anjali>
2025-12-30 08:37:10 +0530

    Day 1: Two Sum optimized using HashMap

--- Array/Easy/two_Sum_LC1.java
+++ Array/Easy/two_Sum_LC1.java
@@ -1,17 +1,20 @@
+
 // Problem: LeetCode 1 - Two Sum
 // Approach: Brute Force
 // Time Complexity: O(n^2)
 // Space Complexity: O(1)
 // Learning: Must check all index pairs, not just adjacent ones
+import java.util.HashMap;
 
 public class two_Sum_LC1 {
     public static void main(String args[]) {
         int nums[] = { 2, 7, 11, 15 };
-        int target = 8;
-        int result[] = twoSum(nums, target);
-       for(int i: result){
-        System.out.print(i+" ");
-       }
+        int target = 18;
+        // int result[] = twoSum(nums, target);
+        int result[] = hashtwoSum(nums, target);
+        for (int i : result) {
+            System.out.print(i + " ");
+        }
 
     }
 
@@ -23,6 +26,19 @@ public class two_Sum_LC1 {
                 }
             }
 
+        }
+        return new int[] { -1, -1 };
+    }
+
+    // using hashmap optimization
+    static int[] hashtwoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int need = target - nums[i];
+            if (map.containsKey(need)) {
+                return new int[] { map.get(need), i };
+            }
+            map.put(nums[i], i);
         }
         return new int[] { -1, -1 };
     }
